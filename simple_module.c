7504580f97108e827f5a7e4f50588e03b9c0dfd8
rlacksgus97 <33649903+rlacksgus97@users.noreply.github.com>
2021-12-06 15:58:29 +0900

    Update simple_module.c

--- simple_module.c
+++ simple_module.c
@@ -13,8 +13,19 @@ struct my_list_head {
 	container_of(ptr, type, member)
 
 #define list_for_each(pos, head) \
-	for (pos = (head)->next; pos != (head); pos = pos->next)
+	for (pos = (head)->next_i; pos != NULL; pos = pos->next_i)
+
+#define list_for_each_entry_index(pos, head, member)				\
+	for (pos = list_first_entry_index(head, typeof(*pos), member);	\
+	     &pos->member != NULL;					\
+	     pos = list_next_entry_index(pos, member))
+	     
+#define list_first_entry_index(ptr, type, member) \
+	list_entry((ptr)->next_i, type, member)
 	
+#define list_next_entry_index(pos, member) \
+	list_entry((pos)->member.next_i, typeof(*(pos)), member)
+
 #define list_first_entry_left(ptr, type, member) \
 	list_entry((ptr)->prev_n, type, member)
 	
@@ -57,13 +68,35 @@ static inline void list_insert_by_value(struct my_list *pos, struct my_list_head
 	struct my_list_head *new = &(new_node->list);
 	
 	if (new_value%2 == 1 && head->prev_n==head) {
+		if (head->next_n == head) {
+			new->next_i = NULL;
+			new->prev_i = head;
+			head->next_i = new;
+		}
+		else {
+			head->next_n->next_i = new;
+			new->prev_i = head->next_n;
+			new->next_i = NULL;
+		}
 		head->prev_n = new;
 		new->next_n = head;
+		new->prev_n = NULL;
 		return 0;
 	}
 	if (new_value%2 == 0 && head->next_n==head){
+		if (head->prev_n == head) {
+			new->next_i = NULL;
+			new->prev_i = head;
+			head->next_i = new;
+		}
+		else {
+			head->prev_n->next_i = new;
+			new->prev_i = head->prev_n;
+			new->next_i = NULL;
+		}
 		head->next_n = new;
 		new->prev_n = head;
+		new->next_n = NULL;
 		return 0;
 	}
 	
@@ -140,52 +173,25 @@ static inline void list_insert_by_value(struct my_list *pos, struct my_list_head
 }
 
 static inline void list_delete_by_value(struct my_list *pos, struct my_list_head *head, int value) {
-	struct my_list *new_node = kmalloc(sizeof(struct my_list), GFP_KERNEL);
-	new_node->num = new_value;
-	struct my_list_head *new = &(new_node->list);
-	
-	if (new_value%2 == 1 && head->prev_n==head) {
-		head->prev_n = new;
-		new->next_n = head;
-		return 0;
-	}
-	if (new_value%2 == 0 && head->next_n==head){
-		head->next_n = new;
-		new->prev_n = head;
-		return 0;
-	}
-	
 	if (value%2 == 1) {
 		list_for_each_entry_left(pos, head, list){
 			if (pos->num == value) {
-				//just put in
-				if(new_value%2==1){
-					head->prev_n->next_n = new;
-					new->prev_n = head->prev_n;
-					
-					head->prev_n = new;
-					new->next_n = head;
+				//delete from n
+				struct my_list_head *pos_original_next_n = pos->list.next_n;
+				if (pos->list.prev_n != NULL){
+					pos->list.prev_n->next_n = pos_original_next_n;
+				}
+				if (pos_original_next_n !=NULL) {
+					pos_original_next_n->prev_n = pos->list.prev_n;
 				}
 				
-				if(new_value%2==0){
-					head->next_n->prev_n = new;
-					new->next_n = head->next_n;
-					
-					head->next_n = new;
-					new->prev_n = head;
-				
-				}
-				
-				//bind pos_node and new_node for real index
-				struct my_list_head *pos_original_next = pos->list.next_i;
-				pos->list.next_i = new;
-				new->prev_i = &(pos->list);
-				
-				new->next_i = pos_original_next;
-				if (pos_original_next != NULL){
-					pos_original_next->prev_i = new;
-				}
-				
+				//delete from i
+				struct my_list_head *pos_original_next_i = pos->list.next_i;
+				pos->list.prev_i->next_i = pos_original_next_i;
+				if (pos_original_next_i !=NULL) {
+					pos_original_next_i->prev_i = pos->list.prev_i;
+				}				
+				kfree(pos);
 				break;
 			}
 		}
@@ -193,34 +199,41 @@ static inline void list_delete_by_value(struct my_list *pos, struct my_list_head
 	else {
 		list_for_each_entry_right(pos, head, list){
 			if (pos->num == value) {
-				//just put in
-				if(new_value%2==1){
-					head->prev_n->next_n = new;
-					new->prev_n = head->prev_n;
-					
-					head->prev_n = new;
-					new->next_n = head;
+				//delete from n
+				struct my_list_head *pos_original_next_n = pos->list.next_n;
+				if (pos->list.prev_n != NULL){
+					pos->list.prev_n->next_n = pos_original_next_n;
+				}
+				if (pos_original_next_n !=NULL) {
+					pos_original_next_n->prev_n = pos->list.prev_n;
 				}
 				
-				if(new_value%2==0){
-					head->next_n->prev_n = new;
-					new->next_n = head->next_n;
-					
-					head->next_n = new;
-					new->prev_n = head;
-				
-				}
-				
-				//bind pos_node and new_node for real index
-				struct my_list_head *pos_original_next = pos->list.next_i;
-				pos->list.next_i = new;
-				new->prev_i = &(pos->list);
-				
-				new->next_i = pos_original_next;
-				if (pos_original_next != NULL){
-					pos_original_next->prev_i = new;
-				}
-				
+				//delete from i
+				struct my_list_head *pos_original_next_i = pos->list.next_i;
+				pos->list.prev_i->next_i = pos_original_next_i;
+				if (pos_original_next_i !=NULL) {
+					pos_original_next_i->prev_i = pos->list.prev_i;
+				}				
+				kfree(pos);
+				break;
+			}
+		}
+	}
+}
+
+static inline void list_search_by_value(struct my_list *pos, struct my_list_head *head, int value) {
+	if (value%2==1) {
+		list_for_each_entry_left(pos,head,list){
+			if (pos->num == value){
+				printk("search value %d", value);
+				break;
+			}
+		}
+	}
+	else {
+		list_for_each_entry_right(pos,head,list){
+			if (pos->num == value){
+				printk("search value %d", value);
 				break;
 			}
 		}
@@ -230,7 +243,7 @@ static inline void list_delete_by_value(struct my_list *pos, struct my_list_head
 void test(void){
 	
 	ktime_t start, end;
-	s64 time_new, time_original;
+	s64 time_i, time_s, time_d;
 	//start=ktime_get();
 	//end=ktime_get();
 	//time_new=ktime_to_ns(ktime_sub(end, start));
@@ -243,16 +256,47 @@ void test(void){
 	
 	start=ktime_get();
 	int i = 0;
-	for (i=2; i<10000; i++){
+	for (i=2; i<100000; i++){
 		list_insert_by_value(mylist, &head, i, i+1);
 		//printk("step %d\n", i);	
 	}
 	end=ktime_get();
-	time_new=ktime_to_ns(ktime_sub(end, start));
-	printk("New Insert : %lld ns\n", (long long)time_new);
+	time_i=ktime_to_ns(ktime_sub(end, start));
+	printk("Insert Time: %lld ns\n", (long long)time_i);
+	/*printk("Check Insert Result\n");
+	struct my_list *test1;
+	list_for_each_entry_index(test1, &head, list){
+		printk("traverse by index : %d\n", test1->num);
+	}*/
 	
 	
+	start=ktime_get();
+	list_search_by_value(mylist, &head, 50000);
+	end=ktime_get();
+	time_s=ktime_to_ns(ktime_sub(end, start));
+	printk("Search Time: %lld ns\n", (long long)time_s);
 	
+	start=ktime_get();
+	int j = 0;
+	for (j=3; j<100001; j++){
+		list_delete_by_value(mylist, &head, j);
+		//printk("step %d\n", i);	
+	}
+	end=ktime_get();
+	time_d=ktime_to_ns(ktime_sub(end, start));
+	printk("Delete Time: %lld ns\n", (long long)time_d);
+	/*printk("Delete Insert Result\n");
+	struct my_list *test2;
+	list_for_each_entry_index(test2, &head, list){
+		printk("traverse by index : %d\n", test2->num);
+	}*/
+	
+	/*list_for_each_entry_left(mylist, &head, list){
+		printk("left num : %d\n", mylist->num);
+	}
+	list_for_each_entry_right(mylist, &head, list){
+		printk("right num : %d\n", mylist->num);
+	}*/
 
 }
 
@@ -286,7 +330,6 @@ MODULE_LICENSE("GPL");
 		}, \
 	), \
 	*/
-
 
 
 
